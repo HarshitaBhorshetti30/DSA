8a250bddfb4288c607b6c2b3c00dce46b13811fc
HarshitaBhorshetti30 <77805050+HarshitaBhorshetti30@users.noreply.github.com>
2021-11-14 22:11:22 -0800

    find minimum users

--- Algorithms/kosaraju.py
+++ Algorithms/kosaraju.py
@@ -7,6 +7,7 @@ class Kosaraju:
         self.visited = set()
         self.stack = []
         self.adj_list = adj_list
+        self.compressed_adj = defaultdict(set)
 
     def DFS1(self, v):
         for n in self.adj_list[v]:
@@ -32,32 +33,49 @@ class Kosaraju:
 
     def findSCC(self):
         ans = []
-
+    
         # Perform DFS 
         for v in self.adj_list:
             if v not in self.visited:
                 self.visited.add(v)
                 self.DFS1(v)
 
-        print(self.stack)
-
         # Reversing Graph
         self.reverseGraph()
         self.visited = set()
 
-        print(self.adj_list)
-
         # Perform DFS for each vertex in stack for SCC
         for i in range(len(self.stack)):
             curr = self.stack.pop()
-            print(curr)
             if curr not in self.visited:
                 self.visited.add(curr)
                 scc = self.DFS2(curr)
-                print(scc)
                 ans.append(scc)
-
         return ans
+
+    def findMinimumUsers(self, ans):
+        users = []
+        incoming, who = dict(), dict()
+
+        # create a who map for representative and actual node
+        for scc in ans:
+            incoming[scc[0]] = 0
+            for v in scc:
+                who[v] = scc[0]
+
+        # create a compressed graph
+        for v in self.adj_list:
+            for neighbor in self.adj_list[v]:
+                if who[v] != who[neighbor] and who[neighbor] not in self.compressed_adj[who[v]]:
+                    self.compressed_adj[who[v]].add(who[neighbor])
+                    incoming[who[neighbor]] += 1
+
+        # find users with incoming edge 0
+        for v in incoming:
+            if incoming[v] == 0:
+                users.append(v)
+
+        return users
 
 
 adj_list = {
@@ -71,4 +89,7 @@ adj_list = {
 }
 
 k = Kosaraju(adj_list)
-print(k.findSCC())
+scc = k.findSCC()
+print(scc)
+print(k.findMinimumUsers(scc))
+
